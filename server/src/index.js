e9511b614209fea6b0ae9a63314948f8fdebea57
Sagi <sagi403@gmail.com>
2022-04-26 15:58:53 +0300

    Setup dockerfile and mongoose connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,16 @@
 import mongoose from "mongoose";
 import { app } from "./app.js";
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://face-mongo-srv:27017/face");
+    console.log("Connected to mongoDb");
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectDB();
+
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
 });
--- server/src/routes/add-person.js
+++ server/src/routes/add-person.js
@@ -5,8 +5,16 @@ import generateFaceFeatures from "../utils/generate-face-features.js";
 const router = express.Router();
 
 router.post("/api/add-person", async (req, res) => {
-  const faceFeatures = generateFaceFeatures();
-  res.status(201).send({ faceFeatures });
+  const name = req.body;
+  const vector = generateFaceFeatures();
+
+  const face = new Face({
+    name,
+    vector,
+  });
+  await face.save();
+
+  res.status(201).send(face);
 });
 
 export { router as addPersonRouter };
