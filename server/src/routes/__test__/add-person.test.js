280535a3e6596511c82574692aacf6b5874dbd0e
Sagi <sagi403@gmail.com>
2022-04-27 14:43:43 +0300

    Testing add-person route parameters validation

--- server/src/routes/__test__/add-person.test.js
+++ server/src/routes/__test__/add-person.test.js
@@ -9,3 +9,19 @@ it("has a route handler listening to /api/add-person for post request", async ()
 
   expect(response.status).not.toEqual(404);
 });
+
+it("create a person in the face db with valid parameters", async () => {
+  let face = await Face.find({});
+  expect(face.length).toEqual(0);
+
+  await request(app)
+    .post("/api/add-person")
+    .send({ name: "TestName" })
+    .expect(201);
+
+  face = await Face.find({});
+
+  expect(face.length).toEqual(1);
+  expect(face[0].name).toEqual("TestName");
+  expect(face[0].vector.length).toEqual(256);
+});
