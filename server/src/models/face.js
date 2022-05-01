2a2f996b089a7bdf13e939c5092da4962eb51e94
Sagi <sagi403@gmail.com>
2022-05-01 12:35:59 +0300

    Limiting the number of person that can be uploaded

--- server/src/models/face.js
+++ server/src/models/face.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 
-const faceSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const MAX_PERSON_NUMBER = 10000;
+
+const faceSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    features: {
+      type: [Number],
+      required: true,
+    },
   },
-  features: {
-    type: [Number],
-    required: true,
-  },
-});
+  { capped: { size: 10485760, max: MAX_PERSON_NUMBER, autoIndexId: true } }
+);
 
 const Face = mongoose.model("Face", faceSchema);
 
