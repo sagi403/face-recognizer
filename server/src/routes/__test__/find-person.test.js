ca1078c5c0e361464b61c4b885df834140e61323
Sagi <sagi403@gmail.com>
2022-05-01 11:47:27 +0300

    Testing db collection length

--- server/src/routes/__test__/find-person.test.js
+++ server/src/routes/__test__/find-person.test.js
@@ -8,7 +8,7 @@ it("has a route handler listening to /api/find-person for get request", async ()
   expect(response.status).not.toEqual(404);
 });
 
-it("create a valid request", async () => {
+it("db length larger than min required", async () => {
   let face = await Face.find({});
   expect(face.length).toEqual(0);
 
@@ -40,4 +40,26 @@ it("create a valid request", async () => {
   const response = await request(app).get("/api/find-person").send({});
 
   expect(response.body.length).toEqual(3);
+});
+
+it("db length smaller than min required", async () => {
+  let face = await Face.find({});
+  expect(face.length).toEqual(0);
+
+  await request(app)
+    .post("/api/add-person")
+    .send({ name: "TestName" })
+    .expect(201);
+  await request(app)
+    .post("/api/add-person")
+    .send({ name: "TestName" })
+    .expect(201);
+
+  face = await Face.find({});
+
+  expect(face.length).toEqual(2);
+
+  const response = await request(app).get("/api/find-person").send({});
+
+  expect(response.body.length).toEqual(2);
 });
