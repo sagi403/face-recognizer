dee468944fb0bcc7dd598d2b830683728a61810d
Sagi <sagi403@gmail.com>
2022-05-01 11:42:25 +0300

    Testing valid request

--- server/src/routes/__test__/find-person.test.js
+++ server/src/routes/__test__/find-person.test.js
@@ -7,3 +7,37 @@ it("has a route handler listening to /api/find-person for get request", async ()
 
   expect(response.status).not.toEqual(404);
 });
+
+it("create a valid request", async () => {
+  let face = await Face.find({});
+  expect(face.length).toEqual(0);
+
+  await request(app)
+    .post("/api/add-person")
+    .send({ name: "TestName" })
+    .expect(201);
+  await request(app)
+    .post("/api/add-person")
+    .send({ name: "TestName" })
+    .expect(201);
+  await request(app)
+    .post("/api/add-person")
+    .send({ name: "TestName" })
+    .expect(201);
+  await request(app)
+    .post("/api/add-person")
+    .send({ name: "TestName" })
+    .expect(201);
+  await request(app)
+    .post("/api/add-person")
+    .send({ name: "TestName" })
+    .expect(201);
+
+  face = await Face.find({});
+
+  expect(face.length).toEqual(5);
+
+  const response = await request(app).get("/api/find-person").send({});
+
+  expect(response.body.length).toEqual(3);
+});
